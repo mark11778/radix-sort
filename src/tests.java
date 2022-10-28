1c5d8866c54f4601f52260b3884d6c0751c4456c
Mark Ruzicka <marker11778@gmail.com>
2022-10-27 23:17:47 -0500

    created test file

---
+++ src/tests.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class tests {
+    @Test
+    void ones {
+    i
+    }
+}
